d4478be6e635e9a950530bc6ca9cf29a411aad27
huderdcv <huder.delacruz.27@unsch.edu.pe>
2025-10-01 16:38:09 -0500

    style: modify schedule information

--- src/general/components/schedule-section/SaturdaySchedule.tsx
+++ src/general/components/schedule-section/SaturdaySchedule.tsx
@@ -7,6 +7,7 @@ const saturdayInfo = {
       name: '11:00AM A 1:00PM - SESIÓN DE LA MAÑANA',
       descriptions: [
         'Participan miembros en general y toda persona interesada.',
+        'Alameda y Garcilazo - Jr. 28 de julio 716 (Alameda Valdelirios) - Huamanga',
         'Las Americas y Las Casuarinas - Av. Las Casuarinas #255 - Huamanga',
         'Mariscal Cáceres - Urb Quijano Mendivil Mz. F Lt. 2 - Huamanga',
       ],
@@ -15,6 +16,7 @@ const saturdayInfo = {
       name: '3:00PM A 5:00PM - SESIÓN DE LA TARDE',
       descriptions: [
         'Participan miembros en general y toda persona interesada.',
+        'Alameda y Garcilazo - Jr. 28 de julio 716 (Alameda Valdelirios) - Huamanga',
         'Las Americas y Las Casuarinas - Av. Las Casuarinas #255 - Huamanga',
         'Mariscal Cáceres - Urb Quijano Mendivil Mz. F Lt. 2 - Huamanga',
       ],
@@ -23,6 +25,7 @@ const saturdayInfo = {
       name: '7:00PM A 8:30PM - SESIÓN DE LA NOCHE',
       descriptions: [
         'Participan miembros en general y toda persona interesada.',
+        'Alameda y Garcilazo - Jr. 28 de julio 716 (Alameda Valdelirios) - Huamanga',
         'Las Americas y Las Casuarinas - Av. Las Casuarinas #255 - Huamanga',
         'Mariscal Cáceres - Urb Quijano Mendivil Mz. F Lt. 2 - Huamanga',
         'Huanta - Jr. 2 de mayo 495(Frente al recreo Don Felix) - Huanta',
